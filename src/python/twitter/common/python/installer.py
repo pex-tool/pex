752e07385819b05ec5ed1045ce344c48f4b88564
Brian Wickman <wickman@twitter.com>
2013-07-24 16:56:14 +0000

    [PATCH 050/153] port pants sdist_builder over to use twitter.common.python
    RB_ID=166358
    
    (sapling split of 974bc36cf90064523c3c68ece5f92230119971fa)
    
    (sapling split of d12312311a914b0384fcd7efdc35cb923db1a710)

--- src/python/twitter/common/python/installer.py
+++ src/python/twitter/common/python/installer.py
@@ -14,7 +14,91 @@ from .tracer import TRACER
 from pkg_resources import Distribution, PathMetadata
 
 
-class Installer(object):
+__all__ = (
+  'Installer',
+  'Packager'
+)
+
+
+def after_installation(function):
+  def function_wrapper(self, *args, **kw):
+    self._installed = self.run()
+    if not self._installed:
+      raise Installer.InstallFailure('Failed to install %s' % self._source_dir)
+    return function(self, *args, **kw)
+  return function_wrapper
+
+
+class InstallerBase(object):
+  SETUP_BOOTSTRAP = """
+if '%(setuptools_path)s':
+  import sys
+  sys.path.insert(0, '%(setuptools_path)s')
+  import setuptools
+__file__ = '%(setup_py)s'
+exec(compile(open(__file__).read().replace('\\r\\n', '\\n'), __file__, 'exec'))
+"""
+
+  class InstallFailure(Exception): pass
+
+  def __init__(self, source_dir, strict=True, interpreter=None, install_dir=None):
+    """
+      Create an installer from an unpacked source distribution in source_dir.
+
+      If strict=True, fail if any installation dependencies (e.g. distribute)
+      are missing.
+    """
+    self._source_dir = source_dir
+    self._install_tmp = install_dir or safe_mkdtemp()
+    self._installed = None
+    self._strict = strict
+    self._interpreter = interpreter or PythonInterpreter.get()
+
+  def _setup_command(self):
+    """the setup command-line to run, to be implemented by subclasses."""
+    raise NotImplementedError
+
+  def _postprocess(self):
+    """a post-processing function to run following setup.py invocation."""
+
+  def run(self):
+    if self._installed is not None:
+      return self._installed
+
+    if self._interpreter.distribute is None and self._strict:
+      self._installed = False
+      print('Failed to find distribute in sys.path!', file=sys.stderr)
+      return self._installed
+
+    setup_bootstrap = Installer.SETUP_BOOTSTRAP % {
+      'setuptools_path': self._interpreter.distribute or '',
+      'setup_py': 'setup.py'
+    }
+
+    with TRACER.timed('Installing %s' % self._install_tmp, V=2):
+      command = [self._interpreter.binary, '-']
+      command.extend(self._setup_command())
+      po = subprocess.Popen(command,
+          stdin=subprocess.PIPE,
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE,
+          cwd=self._source_dir)
+      so, se = po.communicate(setup_bootstrap.encode('ascii'))
+      self._installed = po.returncode == 0
+
+    if not self._installed:
+      print('Failed to install stdout:\n%s' % so.decode('utf-8'), file=sys.stderr)
+      print('Failed to install stderr:\n%s' % se.decode('utf-8'), file=sys.stderr)
+      return self._installed
+
+    self._postprocess()
+    return self._installed
+
+  def cleanup(self):
+    safe_rmtree(self._install_tmp)
+
+
+class Installer(InstallerBase):
   """
     Install an unpacked distribution with a setup.py.
 
@@ -37,17 +121,6 @@ class Installer(object):
       '/var/folders/Uh/UhXpeRIeFfGF7HoogOKC+++++TI/-Tmp-/tmpufgZOO/tornado-2.3-py2.6.egg'
   """
 
-  SETUP_BOOTSTRAP = """
-if '%(setuptools_path)s':
-  import sys
-  sys.path.insert(0, '%(setuptools_path)s')
-  import setuptools
-__file__ = '%(setup_py)s'
-exec(compile(open(__file__).read().replace('\\r\\n', '\\n'), __file__, 'exec'))
-"""
-
-  class InstallFailure(Exception): pass
-
   def __init__(self, source_dir, strict=True, interpreter=None):
     """
       Create an installer from an unpacked source distribution in source_dir.
@@ -55,57 +128,19 @@ exec(compile(open(__file__).read().replace('\\r\\n', '\\n'), __file__, 'exec'))
       If strict=True, fail if any installation dependencies (e.g. distribute)
       are missing.
     """
-    self._source_dir = source_dir
-    self._install_tmp = safe_mkdtemp()
-    self._installed = None
-    self._strict = strict
-    self._interpreter = interpreter or PythonInterpreter.get()
+    super(Installer, self).__init__(source_dir, strict=strict, interpreter=interpreter)
+    self._egg_info = None
     fd, self._install_record = tempfile.mkstemp()
     os.close(fd)
 
-  def after_installation(function):
-    def function_wrapper(self, *args, **kw):
-      self._installed = self.run()
-      if not self._installed:
-        raise Installer.InstallFailure('Failed to install %s' % self._source_dir)
-      return function(self, *args, **kw)
-    return function_wrapper
+  def _setup_command(self):
+    return ['install',
+           '--root=%s' % self._install_tmp,
+           '--prefix=',
+           '--single-version-externally-managed',
+           '--record', self._install_record]
 
-  def run(self):
-    if self._installed is not None:
-      return self._installed
-
-    if self._interpreter.distribute is None and self._strict:
-      self._installed = False
-      print('Failed to find distribute in sys.path!', file=sys.stderr)
-      return self._installed
-
-    setup_bootstrap = Installer.SETUP_BOOTSTRAP % {
-      'setuptools_path': self._interpreter.distribute or '',
-      'setup_py': 'setup.py'
-    }
-    with TRACER.timed('Installing %s' % self._install_tmp, V=2):
-      po = subprocess.Popen(
-        [self._interpreter.binary,
-          '-',
-          'install',
-          '--root=%s' % self._install_tmp,
-          '--prefix=',
-          '--single-version-externally-managed',
-          '--record', self._install_record],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        cwd=self._source_dir)
-      so, se = po.communicate(setup_bootstrap.encode('ascii'))
-      self._installed = po.returncode == 0
-    self._egg_info = None
-
-    if not self._installed:
-      print('Failed to install stdout:\n%s' % so.decode('utf-8'), file=sys.stderr)
-      print('Failed to install stderr:\n%s' % se.decode('utf-8'), file=sys.stderr)
-      return self._installed
-
+  def _postprocess(self):
     installed_files = []
     egg_info = None
     with open(self._install_record) as fp:
@@ -146,7 +181,18 @@ exec(compile(open(__file__).read().replace('\\r\\n', '\\n'), __file__, 'exec'))
     metadata = PathMetadata(base_dir, egg_info)
     return Distribution.from_location(base_dir, os.path.basename(egg_info), metadata=metadata)
 
-  def cleanup(self):
-    safe_rmtree(self._install_tmp)
 
-  del after_installation
+class Packager(InstallerBase):
+  """
+    Create a source distribution from an unpacked setup.py-based project.
+  """
+
+  def _setup_command(self):
+    return ['sdist', '--formats=gztar', '--dist-dir=%s' % self._install_tmp]
+
+  @after_installation
+  def sdist(self):
+    dists = os.listdir(self._install_tmp)
+    if len(dists) != 1:
+      raise self.InstallFailure('Ambiguous source distributions found.')
+    return os.path.join(self._install_tmp, dists[0])
