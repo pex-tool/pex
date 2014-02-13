af01a8e1e79747a26ffdb3fc6ae50f81da69697c
Marc Abramowitz <marc@marc-abramowitz.com>
2014-02-12 23:43:39 -0800

    Expose FetchError in twitter/common/python/http/__init__.py
    (sapling split of 8eecbc4a523d45e93dbc8a1237167a4c501e8106)

--- src/python/twitter/common/python/http/__init__.py
+++ src/python/twitter/common/python/http/__init__.py
@@ -1,11 +1,12 @@
 from .crawler import Crawler
-from .http import CachedWeb, Web
+from .http import CachedWeb, FetchError, Web
 from .link import EggLink, Link, SourceLink
 
 __all__ = (
   CachedWeb,
   Crawler,
   EggLink,
+  FetchError,
   Link,
   SourceLink,
   Web,
