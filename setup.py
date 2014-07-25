1c6e61cac6fa6bf54e19969c12f6ce52d0e2bd67
Brian Wickman <wickman@twitter.com>
2014-07-25 13:01:33 -0700

    Make pex an rc0 release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(os.path.dirname(__file__), 'CHANGES')) as fp:
 
 setup(
   name = 'pex',
-  version = '0.7.0',
+  version = '0.7.0-rc0',
   description = "The PEX packaging toolchain.",
   long_description = LONG_DESCRIPTION,
   url = 'https://github.com/pantsbuild/pex',
