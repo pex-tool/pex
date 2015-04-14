cbe202bd392d6274563ef4b384ae88f2b0dd4768
Brian Wickman <wickman@twitter.com>
2015-04-14 11:38:35 -0400

    Fix translator import sorting.

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from pex.translator import ChainedTranslator, TranslatorBase, Translator
+from pex.translator import ChainedTranslator, Translator, TranslatorBase
 
 try:
   import mock
