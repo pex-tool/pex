f07ad9e90ec1e1729c04bfe24bb00908bf2cea7b
Kris Wilson <kwilson@twitter.com>
2016-03-12 13:12:33 -0800

    Revert "Fix egg resolution on windows."
    This reverts commit 1fe5a4ec5d0e201935e4a195729c1f75ae9ef9a9.

--- pex/finders.py
+++ pex/finders.py
@@ -126,7 +126,7 @@ class FixedEggMetadata(pkg_resources.EggMetadata):
   @classmethod
   def normalized_elements(cls, path):
     path_split = path.split('/')
-    while path_split and path_split[-1] in ('', '.'):
+    while path_split[-1] in ('', '.'):
       path_split.pop(-1)
     return path_split
 
@@ -136,9 +136,10 @@ class FixedEggMetadata(pkg_resources.EggMetadata):
     return '/'.join(self.normalized_elements(original_fn))
 
   def _zipinfo_name(self, fspath):
-    # Make sure to not normalize the zip_pre - that's an OS-native path.
-    if fspath.startswith(self.zip_pre):
-      return '/'.join(self.normalized_elements(fspath[len(self.zip_pre):]))
+    fspath = self.normalized_elements(fspath)
+    zip_pre = self.normalized_elements(self.zip_pre)
+    if fspath[:len(zip_pre)] == zip_pre:
+      return '/'.join(fspath[len(zip_pre):])
     assert "%s is not a subpath of %s" % (fspath, self.zip_pre)
 
 
