7db5d2ceb7142b7829d1e243a026a0269c13e6fa
Justin Venus <justin.venus@gmail.com>
2016-11-02 10:20:50 -0500

    scrub Extras from sys.path on macOS Sierra

--- pex/pex.py
+++ pex/pex.py
@@ -94,6 +94,18 @@ class PEX(object):  # noqa: T000
     extras_paths = filter(None, makefile.get('EXTRASPATH', '').split(':'))
     for path in extras_paths:
       yield os.path.join(standard_lib, path)
+    # macOS Sierra support
+    framework = sysconfig.get_config_var("PYTHONFRAMEWORK")
+    if not framework:
+      raise StopIteration()
+    third_party = os.path.join("/Library", framework, sys.version[:3], "site-packages")
+    yield third_party
+    extras_pth = os.path.join(third_party, 'Extras.pth')
+    if not os.path.exists(extras_pth):
+      raise StopIteration()
+    with open(extras_pth, 'r') as fd:
+      for line in fd.readlines():
+        yield line.strip()
 
   @classmethod
   def _get_site_packages(cls):
