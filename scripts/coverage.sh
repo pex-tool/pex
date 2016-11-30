7503658151d0ac85c059ef6fa29e692ebe5c2f43
Kris Wilson <kwilson@twitter.com>
2016-11-30 13:43:54 -0800

    Bump to setuptools 5.7

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,4 +4,4 @@ coverage run -p -m py.test tests
 coverage run -p -m pex.bin.pex -v --help >&/dev/null
 coverage run -p -m pex.bin.pex -v -- scripts/do_nothing.py
 coverage run -p -m pex.bin.pex -v requests -- scripts/do_nothing.py
-coverage run -p -m pex.bin.pex -v . 'setuptools>=5.4.1,<20.11' -- scripts/do_nothing.py
+coverage run -p -m pex.bin.pex -v . 'setuptools>=5.7,<20.11' -- scripts/do_nothing.py
