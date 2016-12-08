222cb22a1fa8c1a816dde91c4ec9baa7165e58c3
Benjy <benjyw@gmail.com>
2016-12-07 19:42:53 -0800

    Release 1.1.17.

--- pex/version.py
+++ pex/version.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-__version__ = '1.1.16'
+__version__ = '1.1.17'
 
 SETUPTOOLS_REQUIREMENT = 'setuptools>=5.7,<31.0'
 WHEEL_REQUIREMENT = 'wheel>=0.26.0,<0.30.0'
