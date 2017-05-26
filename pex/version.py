0d01ba87e0d11590ea2dde03d5d30ce0cebf61d2
cheister <cheister@gmail.com>
2017-05-25 17:59:00 -0700

    Minimum setuptools version should be 20.3 so Requirement has specifier attribute required by PR 355 (#391)
    In #355 package.py was updated to assume that the Requirement class has the specifier attribute.
    
    The specifier attribute wasn't added to the Requirement class until setuptools 20.3 pypa/setuptools@de4bdcf
    
    The error when there is an older setuptools is
    
    Exception message: Requirement instance has no attribute 'specifier'

--- pex/version.py
+++ pex/version.py
@@ -6,6 +6,6 @@ __version__ = '1.2.7'
 # NB: If we upgrade to setuptools>=34 pex's bootstrap code in `PEXBuilder` will need an update to
 # include the `packaging` package in the `.bootstrap/` code since we use
 # `packaging.specifiers.SpecifierSet` - indirectly - through `pkg_resources.Requirement.specifier`.
-SETUPTOOLS_REQUIREMENT = 'setuptools>=20.0,<34.0'
+SETUPTOOLS_REQUIREMENT = 'setuptools>=20.3,<34.0'
 
 WHEEL_REQUIREMENT = 'wheel>=0.26.0,<0.30.0'
