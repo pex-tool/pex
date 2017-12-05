4417f2edcd7908bd90cce140e365b7b81189346c
Chris Livingston <clivingston@twitter.com>
2017-12-04 17:55:43 -0800

    Bug fix for entry point targeting + integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -547,3 +547,23 @@ def test_pex_python():
     assert rc == 0
     correct_interpreter_path = pex_python.encode()
     assert correct_interpreter_path in stdout
+
+
+def test_entry_point_targeting():
+  """Test bugfix for https://github.com/pantsbuild/pex/issues/434"""
+  with temporary_dir() as td:
+    pexrc_path = os.path.join(td, '.pexrc')
+    with open(pexrc_path, 'w') as pexrc:
+      pex_python = ensure_python_interpreter('3.6.3')
+      pexrc.write("PEX_PYTHON=%s" % pex_python)
+
+    # test PEX_PYTHON with valid constraints
+    pex_out_path = os.path.join(td, 'pex.pex')
+    res = run_pex_command(['--disable-cache',
+      'autopep8',
+      '-e', 'autopep8',
+      '-o', pex_out_path])
+    res.assert_success()
+
+    stdout, rc = run_simple_pex(pex_out_path)
+    assert 'usage: autopep8'.encode() in stdout
