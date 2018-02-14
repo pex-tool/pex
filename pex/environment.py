c629357e87c22fd2f88d87a6eebc813281f42d37
Daniel Wagner-Hall <dwagnerhall@twitter.com>
2018-02-14 11:50:10 +0000

    Prepend sys.path entries for bundled dependencies

--- pex/environment.py
+++ pex/environment.py
@@ -192,6 +192,18 @@ class PEXEnvironment(Environment):
 
         if os.path.isdir(dist.location):
           with TRACER.timed('Adding sitedir', V=2):
+            if dist.location not in sys.path:
+              # Prepend location to sys.path.
+              # This ensures that bundled versions of libraries will be used before system-installed
+              # versions, in case something is installed in both, helping to favor hermeticity in
+              # the case of non-hermetic PEX files (i.e. those with inherit_path=True).
+              #
+              # If the path is not already in sys.path, site.addsitedir will append (not prepend)
+              # the path to sys.path. But if the path is already in sys.path, site.addsitedir will
+              # leave sys.path unmodified, but will do everything else it would do. This is not part
+              # of its advertised contract (which is very vague), but has been verified to be the
+              # case by inspecting its source for both cpython 2.7 and cpython 3.7.
+              sys.path = [dist.location] + sys.path
             site.addsitedir(dist.location)
 
         dist.activate()
