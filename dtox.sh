717bbf7594080b91a5eb39f7bbd8ecbaec94ceb9
John Sirois <john.sirois@gmail.com>
2018-06-14 23:13:00 -0600

    Robustify image existence test, include tag in query.
    Also set hash labels in key-value form for better readability
    using docker inspect.

--- dtox.sh
+++ dtox.sh
@@ -10,13 +10,13 @@ BASE_INPUT=(
 base_hash=$(cat ${BASE_INPUT[@]} | git hash-object -t blob --stdin)
 
 function base_id() {
-  docker images -q -f label=${base_hash} pantsbuild/pex
+  docker images -q -f label=base_hash=${base_hash} pantsbuild/pex:base
 }
 
 if [[ -z "$(base_id)" ]]; then
   docker build \
     --tag pantsbuild/pex:base \
-    --label ${base_hash} \
+    --label base_hash=${base_hash} \
     "${ROOT}/docker/base"
 fi
 
@@ -25,7 +25,7 @@ USER_INPUT=(
   "${ROOT}/docker/user/create_docker_image_user.sh"
 )
 user_hash=$(cat ${USER_INPUT[@]} | git hash-object -t blob --stdin)
-if [[ -z "$(docker images -q -f label=${user_hash} pantsbuild/pex)" ]]; then
+if [[ -z "$(docker images -q -f label=user_hash=${user_hash} pantsbuild/pex:user)" ]]; then
   docker build \
     --build-arg BASE_ID=$(base_id) \
     --build-arg USER=$(id -un) \
@@ -33,7 +33,7 @@ if [[ -z "$(docker images -q -f label=${user_hash} pantsbuild/pex)" ]]; then
     --build-arg GROUP=$(id -gn) \
     --build-arg GID=$(id -g) \
     --tag pantsbuild/pex:user \
-    --label ${user_hash} \
+    --label user_hash=${user_hash} \
     "${ROOT}/docker/user"
 fi
 
