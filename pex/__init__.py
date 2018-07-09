fec3647c15586ba520ebea4582d39188e02752ea
wisechengyi <wisechengyi@gmail.com>
2018-07-08 17:10:02 -0700

    rev init

--- pex/__init__.py
+++ pex/__init__.py
@@ -1,1 +1,2 @@
-__import__('pkg_resources').declare_namespace(__name__)
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
