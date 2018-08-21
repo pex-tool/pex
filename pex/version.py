b35a1c69b92fd0bdf6b8e545f044010c353ff8de
John Sirois <john.sirois@gmail.com>
2018-08-21 13:43:32 -0600

    Inline exclusions in the setuptools requirement.

--- pex/version.py
+++ pex/version.py
@@ -5,8 +5,6 @@ __version__ = '1.4.5'
 
 # Versions 34.0.0 through 35.0.2 (last pre-36.0.0) de-vendored dependencies which causes problems
 # for pex code so we exclude that range.
-__exclusions = '!=34.*,!=35.*'
-SETUPTOOLS_REQUIREMENT = 'setuptools>=20.3,<41,{exclusions}'.format(exclusions=__exclusions)
-del __exclusions
+SETUPTOOLS_REQUIREMENT = 'setuptools>=20.3,<41,!=34.*,!=35.*'
 
 WHEEL_REQUIREMENT = 'wheel>=0.26.0,<0.32'
