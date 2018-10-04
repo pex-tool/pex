189b0db309f637f9ed3ef3c35e74312329c79300
John Sirois <john.sirois@gmail.com>
2018-10-04 15:47:48 -0400

    Prepare the 1.4.8 release. (#577)
    Fixes #560

--- pex/version.py
+++ pex/version.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-__version__ = '1.4.7'
+__version__ = '1.4.8'
 
 # Versions 34.0.0 through 35.0.2 (last pre-36.0.0) de-vendored dependencies which causes problems
 # for pex code so we exclude that range.
