6da464625ddc8fd47a68a702e455c53413f65f77
John Sirois <john.sirois@gmail.com>
2018-10-29 16:53:30 -0600

    Fix pex force local to handle PEP-420.
    Previously, pex would blow up trying to adjust import paths if a PEP-420
    implicit namespace package was encountered. Add a test of the path
    adjustment functionality both to confirm it was needed (it was) and that
    the fix technique works with all forms of namespace packages by only
    assuming they support `append`.
    
    To support all this, work around a pypy zipimporter bug:
    https://bitbucket.org/pypy/pypy/issues/1686 and, as a side-effect,
    eliminate our CI workaround for the pypy unit test shard.
    
    Fixes #598
    Fixes #497

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,14 +1,16 @@
 # Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
 import os
 import platform
 import subprocess
 from contextlib import contextmanager
+from textwrap import dedent
 
 import pytest
 
 from pex import resolver, vendor
-from pex.compatibility import nested, to_bytes
+from pex.compatibility import PY2, nested, to_bytes
 from pex.environment import PEXEnvironment
 from pex.installer import EggInstaller, WheelInstaller
 from pex.interpreter import PythonInterpreter
@@ -16,7 +18,15 @@ from pex.package import SourcePackage, WheelPackage
 from pex.pex import PEX
 from pex.pex_builder import PEXBuilder
 from pex.pex_info import PexInfo
-from pex.testing import make_bdist, temporary_dir, temporary_filename
+from pex.resolver import resolve
+from pex.testing import (
+    PY35,
+    ensure_python_interpreter,
+    make_bdist,
+    temporary_content,
+    temporary_dir,
+    temporary_filename
+)
 
 
 @contextmanager
@@ -47,6 +57,143 @@ def test_force_local():
 
     # idempotence
     assert PEXEnvironment.force_local(pex_file, pb.info) == code_cache
+
+
+def assert_force_local_implicit_ns_packages_issues_598(interpreter=None,
+                                                       requirements=(),
+                                                       create_ns_packages=True):
+
+  def create_foo_bar_setup(name, **extra_args):
+    setup_args = dict(
+      name=name,
+      version='0.0.1',
+      packages=['foo', 'foo.bar']
+    )
+    if create_ns_packages:
+      setup_args.update(namespace_packages=['foo', 'foo.bar'])
+    if requirements:
+      setup_args.update(install_requires=list(requirements))
+    setup_args.update(extra_args)
+
+    return dedent("""
+      from setuptools import setup
+
+      setup(**{setup_args!r})
+    """.format(setup_args=setup_args))
+
+  def with_foo_bar_ns_packages(content):
+    ns_packages = {
+      os.path.join(pkg, '__init__.py'): '__import__("pkg_resources").declare_namespace(__name__)'
+      for pkg in ('foo', 'foo/bar')
+    } if create_ns_packages else {}
+    ns_packages.update(content)
+    return ns_packages
+
+  content1 = with_foo_bar_ns_packages({
+    'foo/bar/spam.py': 'identify = lambda: 42',
+    'setup.py': create_foo_bar_setup('foo-bar-spam')
+  })
+
+  content2 = with_foo_bar_ns_packages({
+    'foo/bar/eggs.py': dedent("""
+      # NB: This only works when this content is unpacked loose on the filesystem!
+      def read_self():
+        with open(__file__) as fp:
+          return fp.read()
+    """)
+  })
+
+  content3 = with_foo_bar_ns_packages({
+    'foobaz': dedent("""\
+      #!python
+      import sys
+
+      from foo.bar import baz
+
+      sys.exit(baz.main())
+    """),
+    'foo/bar/baz.py': dedent("""
+      import sys
+
+      from foo.bar import eggs, spam
+
+      def main():
+        assert len(eggs.read_self()) > 0
+        return spam.identify()
+    """),
+    'setup.py': create_foo_bar_setup('foo-bar-baz', scripts=['foobaz'])
+  })
+
+  def add_requirements(builder, cache):
+    for resolved_dist in resolve(requirements, cache=cache, interpreter=builder.interpreter):
+      builder.add_requirement(resolved_dist.requirement)
+      builder.add_distribution(resolved_dist.distribution)
+
+  def add_wheel(builder, content):
+    with temporary_content(content) as project:
+      dist = WheelInstaller(project, interpreter=builder.interpreter).bdist()
+      builder.add_dist_location(dist)
+
+  def add_sources(builder, content):
+    with temporary_content(content) as project:
+      for path in content.keys():
+        builder.add_source(os.path.join(project, path), path)
+
+  interpreter = vendor.setup_interpreter(interpreter=interpreter)
+  with nested(temporary_dir(), temporary_dir()) as (root, cache):
+    pex_info1 = PexInfo.default()
+    pex_info1.zip_safe = False
+    pex1 = os.path.join(root, 'pex1.pex')
+    builder1 = PEXBuilder(interpreter=interpreter, pex_info=pex_info1)
+    add_requirements(builder1, cache)
+    add_wheel(builder1, content1)
+    add_sources(builder1, content2)
+    builder1.build(pex1)
+
+    pex_info2 = PexInfo.default()
+    pex_info2.pex_path = pex1
+    pex2 = os.path.join(root, 'pex2')
+    builder2 = PEXBuilder(path=pex2, interpreter=interpreter, pex_info=pex_info2)
+    add_requirements(builder2, cache)
+    add_wheel(builder2, content3)
+    builder2.set_script('foobaz')
+    builder2.freeze()
+
+    assert 42 == PEX(pex2, interpreter=interpreter).run(env=dict(PEX_VERBOSE='9'))
+
+
+@pytest.fixture
+def setuptools_requirement():
+  # We use a very old version of setuptools to prove the point the user version is what is used
+  # here and not the vendored version (when possible). A newer setuptools is needed though to work
+  # with python 3.
+  return 'setuptools==1.0' if PY2 else 'setuptools==17.0'
+
+
+def test_issues_598_explicit_any_interpreter(setuptools_requirement):
+  assert_force_local_implicit_ns_packages_issues_598(requirements=[setuptools_requirement],
+                                                     create_ns_packages=True)
+
+
+def test_issues_598_explicit_missing_requirement():
+  assert_force_local_implicit_ns_packages_issues_598(create_ns_packages=True)
+
+
+@pytest.fixture
+def python_35_interpreter():
+  # Python 3.5 supports implicit namespace packages.
+  return PythonInterpreter.from_binary(ensure_python_interpreter(PY35))
+
+
+def test_issues_598_implicit(python_35_interpreter):
+  assert_force_local_implicit_ns_packages_issues_598(interpreter=python_35_interpreter,
+                                                     create_ns_packages=False)
+
+
+def test_issues_598_implicit_explicit_mixed(python_35_interpreter, setuptools_requirement):
+  assert_force_local_implicit_ns_packages_issues_598(interpreter=python_35_interpreter,
+                                                     requirements=[setuptools_requirement],
+                                                     create_ns_packages=True)
 
 
 def normalize(path):
