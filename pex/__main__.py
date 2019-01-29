27c77e303ce0f10038ce7db33f06243acb10854a
John Sirois <john.sirois@gmail.com>
2019-01-29 12:27:06 -0700

    Update __main__.py
    Add copyright header and fix isort failure.

--- pex/__main__.py
+++ pex/__main__.py
@@ -1,4 +1,8 @@
-from pex.bin import pex
+# Copyright 2019 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+from __future__ import absolute_import
+
+from pex.bin import pex
 
 __name__ == '__main__' and pex.main()
