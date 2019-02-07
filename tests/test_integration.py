b69ad2aa12a375ac5de5681af69b1ff8aeb15f8a
John Sirois <john.sirois@gmail.com>
2019-02-06 20:23:13 -0700

    Add a test for the issue.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1205,3 +1205,16 @@ def test_force_local_implicit_ns_packages_issues_598():
 
     subprocess.check_call([tcl_pex, '-c', 'from twitter.common.lang import Singleton'],
                           env=make_env(PEX_FORCE_LOCAL='1', PEX_PATH=tcl_pex))
+
+
+def test_issues_661_devendoring_required():
+  # The cryptography distribution does not have a whl released for python3 on linux at version 2.5.
+  # As a result, we're forced to build it under python3 and, prior to the fix for
+  # https://github.com/pantsbuild/pex/issues/661, this would fail using the vendored setuptools
+  # inside pex.
+  with temporary_dir() as td:
+    cryptography_pex = os.path.join(td, 'cryptography.pex')
+    res = run_pex_command(['cryptography==2.5', '-o', cryptography_pex])
+    res.assert_success()
+
+    subprocess.check_call([cryptography_pex, '-c', 'import cryptography'])
