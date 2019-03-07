f058d1b6ad8836098cb553c3b8153c2e9e049f0c
Eric Arellano <ericarellano@me.com>
2019-03-07 00:46:23 -0700

    Always OR interpreter constraints when given list
    See https://github.com/pantsbuild/pex/issues/655.

--- pex/pex_bootstrapper.py
+++ pex/pex_bootstrapper.py
@@ -85,8 +85,7 @@ def find_compatible_interpreters(pex_python_path, compatibility_constraints):
       # get all qualifying interpreters found in $PATH
       interpreters = PythonInterpreter.all()
 
-  return list(matched_interpreters(
-    interpreters, compatibility_constraints, meet_all_constraints=True))
+  return list(matched_interpreters(interpreters, compatibility_constraints))
 
 
 def _select_pex_python_interpreter(target_python, compatibility_constraints):
@@ -96,7 +95,7 @@ def _select_pex_python_interpreter(target_python, compatibility_constraints):
     die('Failed to find interpreter specified by PEX_PYTHON: %s' % target)
   if compatibility_constraints:
     pi = PythonInterpreter.from_binary(target)
-    if not list(matched_interpreters([pi], compatibility_constraints, meet_all_constraints=True)):
+    if not list(matched_interpreters([pi], compatibility_constraints)):
       die('Interpreter specified by PEX_PYTHON (%s) is not compatible with specified '
           'interpreter constraints: %s' % (target, str(compatibility_constraints)))
   if not os.path.exists(target):
