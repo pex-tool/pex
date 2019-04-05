a4c5d96e16dac892a6d84b02bdb3c0b8e14e9e1b
Fabian Höring <fhoring@gmail.com>
2019-04-05 19:47:53 +0200

    Fix pex file looses the executable permissions of binary files (#703)
    Fixes #223

--- pex/util.py
+++ pex/util.py
@@ -5,14 +5,13 @@ from __future__ import absolute_import
 
 import contextlib
 import os
-import shutil
 import tempfile
 import uuid
 from hashlib import sha1
 from site import makepath
 from threading import Lock
 
-from pex.common import rename_if_empty, safe_mkdir, safe_mkdtemp, safe_open
+from pex.common import rename_if_empty, safe_mkdir, safe_mkdtemp
 from pex.compatibility import exec_function
 from pex.finders import register_finders
 from pex.third_party.pkg_resources import (
@@ -176,11 +175,9 @@ class CacheHelper(object):
       target_dir_tmp = target_dir + '.' + uuid.uuid4().hex
       for name in zf.namelist():
         if name.startswith(source) and not name.endswith('/'):
-          # strip off prefix + '/'
-          target_name = os.path.join(dependency_basename, name[len(source) + 1:])
-          with contextlib.closing(zf.open(name)) as zi:
-            with safe_open(os.path.join(target_dir_tmp, target_name), 'wb') as fp:
-              shutil.copyfileobj(zi, fp)
+          zf.extract(name, target_dir_tmp)
+      os.rename(os.path.join(target_dir_tmp, source),
+                os.path.join(target_dir_tmp, dependency_basename))
 
       rename_if_empty(target_dir_tmp, target_dir)
 
