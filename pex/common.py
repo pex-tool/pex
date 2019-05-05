f7364de731d90a72e4185990cda84f0324a96162
Eric Arellano <ericarellano@me.com>
2019-05-05 10:20:11 -0700

    Convert deterministic_datetime from a function to a constant
    We use the result many times, and it is not expected to ever change between a pex run, so this is better set to be a property.
    
    We eagerly evaluate the constant for simplicity of implementation. Because the calculation is so cheap, it is not necessary to lazily evaluate.

--- pex/common.py
+++ pex/common.py
@@ -19,26 +19,21 @@ from datetime import datetime
 from uuid import uuid4
 
 
-def deterministic_datetime():
-  """Return a deterministic UTC datetime object that does not depend on the system time.
-
-  First try to read from the standard $SOURCE_DATE_EPOCH, then default to midnight January 1,
-  1980, which is the start of time for MS-DOS time. Per section 4.4.6 of the zip spec at
-  https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT, Zipfiles use MS-DOS time. So, we
-  use this default to ensure no issues with Zipfiles.
-
-  Even though we use MS-DOS to inform the default value, note that we still return a normal UTC
-  datetime object for flexibility with how the result is used.
-
-  For more information on $SOURCE_DATE_EPOCH, refer to
-  https://reproducible-builds.org/docs/source-date-epoch/."""
-  return (
-    datetime.utcfromtimestamp(int(os.environ['SOURCE_DATE_EPOCH']))
-    if "SOURCE_DATE_EPOCH" in os.environ
-    else datetime(
-      year=1980, month=1, day=1, hour=0, minute=0, second=0, microsecond=0, tzinfo=None
-    )
+# We resolve a deterministic UTC datetime object that does not depend on the system time.
+# * First try to read from the standard $SOURCE_DATE_EPOCH. See
+#   https://reproducible-builds.org/docs/source-date-epoch/.
+# * Fallback to midnight January 1, 1980, which is the start of MS-DOS time. Per section
+#   4.4.6 of the zip spec at https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT,
+#   Zipfiles use MS-DOS time.
+# * Even though we use MS-DOS to inform the default value, note that we still return a normal
+#   UTC datetime object for flexibility with how the result is used.
+DETERMINISTIC_DATETIME = (
+  datetime.utcfromtimestamp(int(os.environ['SOURCE_DATE_EPOCH']))
+  if "SOURCE_DATE_EPOCH" in os.environ
+  else datetime(
+    year=1980, month=1, day=1, hour=0, minute=0, second=0, microsecond=0, tzinfo=None
   )
+)
 
 
 def die(msg, exit_code=1):
@@ -412,7 +407,7 @@ class Chroot(object):
           zinfo = zipfile.ZipInfo(
             filename=arcname,
             # NB: the constructor expects a six tuple of year-month-day-hour-minute-second.
-            date_time=deterministic_datetime().timetuple()[:6]
+            date_time=DETERMINISTIC_DATETIME.timetuple()[:6]
           )
           zinfo.external_attr = (st.st_mode & 0xFFFF) << 16
           if isdir:
