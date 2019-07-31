5543f326f47ff2d5c1e6e827873e298f533f0ef8
Josh Hamet <jhamet@twitter.com>
2019-07-30 19:35:55 -0700

    Add integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1357,6 +1357,36 @@ def test_undeclared_setuptools_import_on_pex_path():
     res.assert_success()
     assert res.output.strip() == 'bigquery version: 1.10.0'
 
+def test_pkg_resource_early_import_on_pex_path():
+  """Test that packages which access pkg_resources at import time can be found with pkg_resources.
+
+  See https://github.com/pantsbuild/pex/issues/749 for context. We only declare
+  namespace packages once all environments have been resolved including ones passed in
+  via PEX_PATH. This avoids importing pkg_resources too early which is potentially impactful
+  with packages interacting with pkg_resources at import time.
+  """
+  with temporary_dir() as td:
+
+    six_pex = os.path.join(td, 'six.pex')
+    run_pex_command(['six', '-o', six_pex]).assert_success()
+
+    src_dir = os.path.join(td, 'src')
+    os.mkdir(src_dir)
+
+    src_file = os.path.join(src_dir, 'execute_import.py')
+    with open(src_file, 'w') as fp:
+      fp.write(dedent("""\
+        import pkg_resources
+        import sys
+
+        pkg_resources.get_distribution('six')
+        """))
+
+    setuptools_pex = os.path.join(td, 'autopep8.pex')
+    run_pex_command(['autopep8', 'setuptools', '-D', src_dir, '--entry-point', 'execute_import', '-o', setuptools_pex]).assert_success()
+    _, return_code = run_simple_pex(setuptools_pex, env=make_env(PEX_PATH=six_pex))
+    assert return_code == 0
+
 
 @pytest.mark.skipif(IS_PYPY)
 def test_issues_539_abi3_resolution():
