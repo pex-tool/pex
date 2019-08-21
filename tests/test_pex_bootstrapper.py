ee4e27c6877c9a349c92e037d5dd7d1b1d06251c
Jordan Liviero <jordan.liviero@arcticwolf.com>
2019-08-21 13:18:25 -0400

    Added some unit tests for pex_bootstrapper and an empty .pex file as a test fixture

--- tests/test_pex_bootstrapper.py
+++ tests/test_pex_bootstrapper.py
@@ -1,11 +1,21 @@
 # Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
+import json
+import os
 import pytest
 
+# mock lives in a different place between Py2 and Py3
+try:
+  from unittest import mock
+except ImportError:
+  import mock
+
 from pex.interpreter import PythonInterpreter
-from pex.pex_bootstrapper import find_compatible_interpreters
+from pex.pex_bootstrapper import find_compatible_interpreters, _bootstrap
 from pex.testing import IS_PYPY, PY27, PY35, PY36, ensure_python_interpreter
+
+EMPTY_FIXTURE_PEX = os.path.join(os.path.dirname(__file__), 'empty_test_fixture.pex')
 
 
 @pytest.mark.skipif(IS_PYPY)
@@ -37,3 +47,47 @@ def test_find_compatible_interpreters():
 
   interpreters = find_compatible_interpreters(compatibility_constraints=['<3'])
   assert set(interpreters).issubset(all_known_interpreters)
+
+
+def test_bootstrap():
+  """Simple test against an empty .pex file"""
+  pex_info = _bootstrap(EMPTY_FIXTURE_PEX)
+  # code_hash is arbitrary; it's just what was written into the test fixture.
+  # It is only here to ensure that we're actually reading the file, and not
+  # returning a defaults-only PexInfo.
+  assert pex_info.code_hash == 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+  assert pex_info.requirements == []
+  assert pex_info.zip_safe == True
+
+
+def test_bootstrap_with_vars_dict():
+  """Test that extra_vars passed in as a dict are considered"""
+  extra_vars = {
+    "pex_root": "some_dir",
+    "script": "my_dict_test_script"
+  }
+  pex_info = _bootstrap(EMPTY_FIXTURE_PEX, include_pexrc=False, extra_vars=extra_vars)
+  assert pex_info.pex_root == "some_dir"
+  assert pex_info.script == "my_dict_test_script"
+
+
+def test_bootstrap_with_vars_json():
+  """Test that extra_vars passed in as a JSON string are considered"""
+  extra_vars = {
+    "pex_root": "some_dir",
+    "script": "my_json_test_script"
+  }
+  json_vars = json.dumps(extra_vars)
+  pex_info = _bootstrap(EMPTY_FIXTURE_PEX, include_pexrc=False, extra_vars=json_vars)
+  assert pex_info.pex_root == "some_dir"
+  assert pex_info.script == "my_json_test_script"
+
+
+def test_bootstrap_with_pexrc():
+  """Test that pexrc files are considered"""
+  pass
+
+
+def test_bootstrap_with_pexrc_and_vars():
+  """Test that both pexrc files and passed-in extra_vars are considered in a suitable order"""
+  pass
