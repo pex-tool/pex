d38d2cbf48f8dd32d892df91accdf4ffd0689cc0
John Sirois <john.sirois@gmail.com>
2019-12-26 16:29:32 -0800

    Handle OSX /tmp shenanigans in tests.

--- tests/test_third_party.py
+++ tests/test_third_party.py
@@ -13,31 +13,31 @@ from pex.variables import ENV
 
 @contextmanager
 def temporary_pex_root():
-  with temporary_dir() as pex_root, ENV.patch(PEX_ROOT=pex_root) as env:
+  with temporary_dir() as pex_root, ENV.patch(PEX_ROOT=os.path.realpath(pex_root)) as env:
     original_isolated = third_party._ISOLATED
     try:
       third_party._ISOLATED = None
-      yield pex_root, env
+      yield os.path.realpath(pex_root), env
     finally:
       third_party._ISOLATED = original_isolated
 
 
 def test_isolated_pex_root():
   with temporary_pex_root() as (pex_root, _):
-    devendored_chroot = third_party.isolated()
+    devendored_chroot = os.path.realpath(third_party.isolated())
     assert pex_root == os.path.commonprefix([pex_root, devendored_chroot])
 
 
 def test_isolated_idempotent_inprocess():
   with temporary_pex_root():
-    assert third_party.isolated() == third_party.isolated()
+    assert os.path.realpath(third_party.isolated()) == os.path.realpath(third_party.isolated())
 
 
 def test_isolated_idempotent_subprocess():
   with temporary_pex_root() as (_, env):
-    devendored_chroot = third_party.isolated()
+    devendored_chroot = os.path.realpath(third_party.isolated())
     stdout = subprocess.check_output(
       args=[sys.executable, '-c', 'from pex.third_party import isolated; print(isolated())'],
       env=env
     )
-    assert devendored_chroot == stdout.decode('utf-8').strip()
+    assert devendored_chroot == os.path.realpath(stdout.decode('utf-8').strip())
