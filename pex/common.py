2ca0b9739ce6687d6ecbfffa96bc62498624fc72
Eric Arellano <ericarellano@me.com>
2020-09-25 20:03:11 -0700

    Add back self._getpid

--- pex/common.py
+++ pex/common.py
@@ -84,6 +84,7 @@ class MktempTeardownRegistry(object):
         # type: () -> None
         self._registry = cast("DefaultDict[int, Set[str]]", defaultdict(set))
         self._lock = threading.RLock()
+        self._getpid = os.getpid
         self._exists = os.path.exists
         self._rmtree = shutil.rmtree
         atexit.register(self.teardown)
