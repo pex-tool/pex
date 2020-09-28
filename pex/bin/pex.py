7b6b0fcfd80d680db960b1d22c5d04f97380c154
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-09-27 18:20:22 -0700

    revert changes to --platform CLI arg help

--- pex/bin/pex.py
+++ pex/bin/pex.py
@@ -500,11 +500,9 @@ def configure_clp_pex_environment(parser):
         callback=process_platform,
         help="The platform for which to build the PEX. This option can be passed multiple times "
         "to create a multi-platform pex. To use the platform corresponding to the current "
-        "interpreter you can pass `current` "
-        "(which is the default value if this option is not provided). "
-        "To target any other platform you pass a string composed of fields: "
-        "<platform>-<python impl abbr>-<python version>-<abi>. "
-        "These fields stem from wheel name conventions as outlined in "
+        "interpreter you can pass `current`. To target any other platform you pass a string "
+        "composed of fields: <platform>-<python impl abbr>-<python version>-<abi>. These fields "
+        "stem from wheel name conventions as outlined in "
         "https://www.python.org/dev/peps/pep-0427#file-name-convention and influenced by "
         "https://www.python.org/dev/peps/pep-0425. For the current interpreter at {} the full "
         "platform string is {}. To find out more, try `{} --platform explain`.".format(
