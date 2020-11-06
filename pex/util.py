552b1582dcb69dd6195f8d4015817b9d21ba812b
John Sirois <john.sirois@gmail.com>
2020-11-06 09:26:50 -0800

    Fix runtime vendoring bytecode compilation races.
    Fixes #1098

--- pex/util.py
+++ pex/util.py
@@ -102,6 +102,27 @@ class DistributionHelper(object):
         return None
 
 
+def filter_pyc_dirs(dirs):
+    # type: (Iterable[str]) -> Iterator[str]
+    """Return an iterator over the input `dirs` filtering out Python bytecode cache directories."""
+    for d in dirs:
+        if d != "__pycache__":
+            yield d
+
+
+def filter_pyc_files(files):
+    # type: (Iterable[str]) -> Iterator[str]
+    """Return an iterator over the input `files` filtering out any Python bytecode files."""
+    for f in files:
+        # For Python 2.7, `.pyc` files are compiled as siblings to `.py` files (there is no
+        # __pycache__ dir). We rely on the fact that the temporary files created by CPython
+        # have object id (integer) suffixes to avoid picking up either finished `.pyc` files
+        # or files where Python bytecode compilation is in-flight; i.e.:
+        # `.pyc.0123456789`-style files.
+        if not re.search(r"\.pyc(?:\.[0-9]+)?$", f):
+            yield f
+
+
 class CacheHelper(object):
     @classmethod
     def update_hash(cls, filelike, digest):
@@ -138,15 +159,9 @@ class CacheHelper(object):
         # type: (str) -> Iterator[str]
         normpath = os.path.realpath(os.path.normpath(directory))
         for root, dirs, files in os.walk(normpath):
-            dirs[:] = [d for d in dirs if d != "__pycache__"]
-            for f in files:
-                # For Python 2.7, `.pyc` files are compiled as siblings to `.py` files (there is no
-                # __pycache__ dir. We rely on the fact that the temporary files created by CPython
-                # have object id (integer) suffixes to avoid picking up either finished `.pyc` files
-                # or files where Python bytecode compilation is in-flight; i.e.:
-                # `.pyc.0123456789`-style files.
-                if not re.search(r"\.pyc(?:\.[0-9]+)?$", f):
-                    yield os.path.relpath(os.path.join(root, f), normpath)
+            dirs[:] = list(filter_pyc_dirs(dirs))
+            for f in filter_pyc_files(files):
+                yield os.path.relpath(os.path.join(root, f), normpath)
 
     @classmethod
     def pex_hash(cls, d):
