2356e285c6a0e0492067c729c85cafa16c42c1b3
John Sirois <john.sirois@gmail.com>
2020-12-04 09:15:07 -0800

    Add a `requires_dists` function.
    This is needed to support #1020 and #1108.

--- pex/dist_metadata.py
+++ pex/dist_metadata.py
@@ -7,11 +7,27 @@ from __future__ import absolute_import
 import email
 
 from pex.third_party.packaging.specifiers import SpecifierSet
-from pex.third_party.pkg_resources import DistInfoDistribution, Distribution
+from pex.third_party.pkg_resources import DistInfoDistribution, Distribution, Requirement
 from pex.typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from typing import Dict, Iterator, Optional
+
+
+_PKG_INFO_BY_DIST = {}  # type: Dict[Distribution, Optional[email.message.Message]]
+
+
+def _parse_pkg_info(dist):
+    # type: (Distribution) -> Optional[email.message.Message]
+    global _PKG_INFO_BY_DIST
+    if dist not in _PKG_INFO_BY_DIST:
+        if not dist.has_metadata(DistInfoDistribution.PKG_INFO):
+            pkg_info = None
+        else:
+            metadata = dist.get_metadata(DistInfoDistribution.PKG_INFO)
+            pkg_info = email.parser.Parser().parsestr(metadata)
+        _PKG_INFO_BY_DIST[dist] = pkg_info
+    return _PKG_INFO_BY_DIST[dist]
 
 
 def requires_python(dist):
@@ -23,12 +39,40 @@ def requires_python(dist):
     :param dist: A distribution to check for `Python-Requires` metadata.
     :return: The required python version specifiers.
     """
-    if not dist.has_metadata(DistInfoDistribution.PKG_INFO):
+    pkg_info = _parse_pkg_info(dist)
+    if pkg_info is None:
         return None
 
-    metadata = dist.get_metadata(DistInfoDistribution.PKG_INFO)
-    pkg_info = email.parser.Parser().parsestr(metadata)
-    python_requirement = pkg_info.get("Requires-Python")
-    if not python_requirement:
+    python_requirement = pkg_info.get("Requires-Python", None)
+    if python_requirement is None:
         return None
     return SpecifierSet(python_requirement)
+
+
+def requires_dists(
+    dist,  # type: Distribution
+    include_1_1_requires=True,  # type: bool
+):
+    # type: (...) -> Iterator[Requirement]
+    """Examines dist for and returns any declared requirements.
+
+    Looks for `Requires-Dist` metadata and, optionally, the older `Requires` metadata if
+    `include_1_1_requires`.
+
+    See:
+    + https://www.python.org/dev/peps/pep-0345/#requires-dist-multiple-use
+    + https://www.python.org/dev/peps/pep-0314/#requires-multiple-use
+
+    :param dist: A distribution to check for requirement metadata.
+    :return: All requirements found.
+    """
+    pkg_info = _parse_pkg_info(dist)
+    if pkg_info is None:
+        return
+
+    for requires_dist in pkg_info.get_all("Requires-Dist", ()):
+        yield Requirement.parse(requires_dist)
+
+    if include_1_1_requires:
+        for requires in pkg_info.get_all("Requires", ()):
+            yield Requirement.parse(requires)
