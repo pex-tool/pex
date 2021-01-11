24f629f41cf5ed7235765301d10e14b665c83446
John Sirois <john.sirois@gmail.com>
2021-01-11 11:16:53 -0800

    Finish typing, go private and rework __eq__ completely.

--- pex/orderedset.py
+++ pex/orderedset.py
@@ -16,7 +16,7 @@ from pex.compatibility import MutableSet
 from pex.typing import TYPE_CHECKING, Generic
 
 if TYPE_CHECKING:
-    from typing import Any, Iterable, Optional, TypeVar
+    from typing import Any, Iterable, Iterator, Optional, TypeVar, Union
 
     _I = TypeVar("_I")
 
@@ -24,21 +24,21 @@ if TYPE_CHECKING:
 class OrderedSet(MutableSet, Generic["_I"]):
     def __init__(self, iterable=None):
         # type: (Optional[Iterable[_I]]) -> None
-        self.map = OrderedDict()  # type: OrderedDict[_I, None]
+        self._data = OrderedDict()  # type: OrderedDict[_I, None]
         if iterable is not None:
             self.update(iterable)
 
     def __len__(self):
         # type: () -> int
-        return len(self.map)
+        return len(self._data)
 
     def __contains__(self, key):
         # type: (Any) -> bool
-        return key in self.map
+        return key in self._data
 
     def add(self, key):
         # type: (_I) -> None
-        self.map[key] = None
+        self._data[key] = None
 
     def update(self, iterable):
         # type: (Iterable[_I]) -> None
@@ -47,21 +47,25 @@ class OrderedSet(MutableSet, Generic["_I"]):
 
     def discard(self, key):
         # type: (_I) -> None
-        self.map.pop(key, None)
+        self._data.pop(key, None)
 
     def __iter__(self):
-        return iter(self.map)
+        # type: () -> Iterator[_I]
+        return iter(self._data)
 
     def __reversed__(self):
-        return reversed(self.map)
+        # type: () -> Iterator[_I]
+        return reversed(self._data)
 
     def pop(self, last=True):
+        # type: (bool) -> _I
         if not self:
             raise KeyError("set is empty")
-        key, _ = self.map.popitem(last=last)
+        key, _ = self._data.popitem(last=last)
         return key
 
     def __repr__(self):
+        # type: () -> str
         if not self:
             return "{}()".format(
                 self.__class__.__name__,
@@ -69,6 +73,7 @@ class OrderedSet(MutableSet, Generic["_I"]):
         return "{}({!r})".format(self.__class__.__name__, list(self))
 
     def __eq__(self, other):
-        if isinstance(other, OrderedSet):
-            return len(self) == len(other) and list(self) == list(other)
-        return set(self) == set(other)
+        # type: (Any) -> Union[bool, NotImplemented]
+        if type(other) != type(self):
+            return NotImplemented
+        return self._data == other._data
