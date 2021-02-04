793d032bacaf937a5b0b30c2db5f25e5082a5f4b
John Sirois <john.sirois@gmail.com>
2021-02-04 14:39:42 -0800

    Upgrade tox and cleanup config.
    Newer tox supports environment expansion which helps DRY up the config.
    Further remove unused and long broken environments and supporting
    scripts.

--- scripts/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-coverage run -p -m pytest tests
-coverage run -p -m pex.bin.pex -v --help >&/dev/null
-coverage run -p -m pex.bin.pex -v -- scripts/do_nothing.py
-coverage run -p -m pex.bin.pex -v requests -- scripts/do_nothing.py
-coverage run -p -m pex.bin.pex -v . 'setuptools>=5.7,<31.0' -- scripts/do_nothing.py
