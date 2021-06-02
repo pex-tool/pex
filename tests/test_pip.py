2d044f735316d6d46d8322923c4fcff59eaaa263
John Sirois <john.sirois@gmail.com>
2021-06-02 09:32:53 -0600

    Fix Pex emitting warnings about its Pip PEX venv.
    Previously we'd see:
    ```
    rm -rf ~/.pex
    $ pex pex -cpex -opex.pex
    /home/jsirois/.venv/pex/lib/python3.9/site-packages/pex/tools/commands/venv.py:141: PEXWarning: Encountered collision building venv at /home/jsirois/.pex/venvs/short/4bd8ddd4 from /home/jsirois/.pex/pip.pex/46820cb5af0dcf9295a4e7f30184cc0e9fa063dc:
    1. /home/jsirois/.pex/venvs/7fbdc9731d96d42f0de338b2dfbc6bbed1c938e7/832e95f85413646e8b7b056ab4e60414f83d7397.01034f69a38943809913ffdfd28dd5bf/lib/python3.9/site-packages/constraints.txt was provided by:
            /home/jsirois/.pex/pip.pex/46820cb5af0dcf9295a4e7f30184cc0e9fa063dc/.deps/setuptools/constraints.txt
            /home/jsirois/.pex/pip.pex/46820cb5af0dcf9295a4e7f30184cc0e9fa063dc/.deps/wheel/constraints.txt
      pex_warnings.warn(message)
    ```

---
+++ tests/test_pip.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import
+
+import os
+import warnings
+
+from pex.interpreter import PythonInterpreter
+from pex.pip import Pip
+from pex.typing import TYPE_CHECKING
+from pex.variables import ENV
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+def test_no_pex_warnings(tmpdir):
+    # type: (Any) -> None
+    pex_root = os.path.join(tmpdir, "pex_root")
+    pip_root = os.path.join(tmpdir, "pip_root")
+    interpreter = PythonInterpreter.get()
+    platform = interpreter.platform
+
+    with ENV.patch(PEX_ROOT=pex_root), warnings.catch_warnings(record=True) as events:
+        pip = Pip.create(path=pip_root, interpreter=interpreter)
+
+    pip.spawn_debug(
+        platform=platform.platform, impl=platform.impl, version=platform.version, abi=platform.abi
+    ).wait()
+
+    assert 0 == len(
+        events
+    ), "Expected no warnings to be emitted when creating a Pip venv but found\n{}".format(
+        "\n".join(map(str, events))
+    )
