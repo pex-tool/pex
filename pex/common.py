57645f9dc75ed0b1b0ee7e1c2af0048b86eb608d
John Sirois <john.sirois@gmail.com>
2021-09-25 13:10:08 -0700

    Introduce a `qualified_name` utility.
    This will be used for error messages when introducing sortability to
    `Enum` and in lock file parsing.
    
    Work towards #1401 and #1414.

--- pex/common.py
+++ pex/common.py
@@ -96,6 +96,28 @@ def pluralize(
         return noun + "s"
 
 
+def qualified_name(item):
+    # type: (Any) -> str
+    """Attempt to produce the fully qualified name for an item.
+
+    If the item is a type, method, property or function, its fully qualified name is returned as
+    best as can be determined. Otherwise, the fully qualified name of the type of the given item is
+    returned.
+
+    :param item: The item to identify.
+    :return: The fully qualified name of the given item.
+    """
+    if isinstance(item, property):
+        item = item.fget
+    if not hasattr(item, "__name__"):
+        item = type(item)
+    return "{module}.{type}".format(
+        module=getattr(item, "__module__", "<unknown module>"),
+        # There is no __qualname__ in Python 2.7; so we do the best we can.
+        type=getattr(item, "__qualname__", item.__name__),
+    )
+
+
 def safe_copy(source, dest, overwrite=False):
     # type: (str, str, bool) -> None
     def do_copy():
