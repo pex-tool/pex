8b8fd9f2a37e00b17047108ea800b1ab85b79c7a
John Sirois <john.sirois@gmail.com>
2022-02-10 19:39:56 -0800

    Respect `PEX_ROOT` in `PEXEnvironment.mount`.
    There was a narrow case of loose layout PEXes that would not mount in
    the `PEX_ROOT` (when set in the environment) that this fixes. That
    narrow case was exercised by the `pip.pex` PEX that Pex itself creates
    to perform resolves with.
    
    Fixes #1598

---
+++ tests/integration/test_issue_1598.py
@@ -0,0 +1,41 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+
+from pex.testing import make_env, run_pex_command
+from pex.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+def test_mount_respects_env(
+    pex_project_dir,  # type: str
+    tmpdir,  # type: Any
+):
+    # type: (...) -> None
+
+    home = os.path.join(str(tmpdir), "home")
+    os.mkdir(home)
+
+    pex_root = os.path.join(home, ".pex")
+    os.mkdir(pex_root)
+    os.chmod(pex_root, 0o555)
+
+    pex_file = os.path.join(str(tmpdir), "pex.pex")
+
+    result = run_pex_command(
+        args=[pex_project_dir, "-o", pex_file], env=make_env(HOME=home), quiet=True
+    )
+    result.assert_success()
+    assert "PEXWarning: PEX_ROOT is configured as {}".format(pex_root) in result.error
+
+    pex_root_override = os.path.join(str(tmpdir), "pex_root_override")
+    result = run_pex_command(
+        args=[pex_project_dir, "-o", pex_file],
+        env=make_env(HOME=home, PEX_ROOT=pex_root_override),
+        quiet=True,
+    )
+    result.assert_success()
+    assert "" == result.error
