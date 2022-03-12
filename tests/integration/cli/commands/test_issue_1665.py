f030634a320d16e53e07b9805690fa97a91677e9
John Sirois <john.sirois@gmail.com>
2022-03-12 13:40:57 -0800

    Fix Locker to prune un-downloaded entries.
    Fixes #1665

---
+++ tests/integration/cli/commands/test_issue_1665.py
@@ -0,0 +1,38 @@
+# Copyright 2022 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import os
+import sys
+
+import pytest
+
+from pex.cli.testing import run_pex3
+from pex.testing import IS_PYPY, run_pex_command
+from pex.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+@pytest.mark.skipif(
+    IS_PYPY or sys.version_info[:3] < (3, 6, 2),
+    reason="The black 22.1.0 distribution requires Python >= 3.6.2",
+)
+def test_lock_black(tmpdir):
+    # type: (Any) -> None
+
+    lock = os.path.join(str(tmpdir), "lock")
+    run_pex3(
+        "lock",
+        "create",
+        "--resolver-version",
+        "pip-2020-resolver",
+        "--style",
+        "universal",
+        "black==22.1.0",
+        "-o",
+        lock,
+    ).assert_success()
+    result = run_pex_command(args=["--lock", lock, "-c", "black", "--", "--version"])
+    result.assert_success()
+    assert " 22.1.0 " in result.output
