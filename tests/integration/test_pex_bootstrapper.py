550d2c1ff6c97f45520014101391a866ae9ebf17
John Sirois <john.sirois@gmail.com>
2022-08-22 14:18:20 -0700

    Try bumping the pyenv cache - this has interpreters built on older macOS I think.

--- tests/integration/test_pex_bootstrapper.py
+++ tests/integration/test_pex_bootstrapper.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
-import itertools
+
 import json
 import os.path
 import re
