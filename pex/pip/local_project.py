82ebed12da1339ff8995750899fdeac3733f4ebb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 05:53:07 +0000

    Adding tarfile member sanitization to extractall()

--- pex/pip/local_project.py
+++ pex/pip/local_project.py
@@ -55,7 +55,26 @@ def digest_local_project(
 
             extract_dir = dest_dir or os.path.join(td, "extracted")
             with tarfile.open(sdist) as tf:
-                tf.extractall(extract_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, extract_dir)
             listing = os.listdir(extract_dir)
             assert len(listing) == 1, (
                 "Expected sdist generated for {directory} to contain one top-level directory, "
