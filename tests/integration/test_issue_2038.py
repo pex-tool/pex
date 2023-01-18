9c7e8526b3dabb7811bd156e1cf8d8df0cc42dc4
John Sirois <john.sirois@gmail.com>
2023-01-18 12:30:41 -0800

    Actually test recursion.

--- tests/integration/test_issue_2038.py
+++ tests/integration/test_issue_2038.py
@@ -29,11 +29,29 @@ def test_wheel_file_url_dep(tmpdir):
     poetry = os.path.join(str(tmpdir), "poetry.pex")
     run_pex_command(args=["poetry==1.3.2", "-c", "poetry", "-o", poetry]).assert_success()
 
+    corelibrary = os.path.join(str(tmpdir), "corelibrary")
+    touch(os.path.join(corelibrary, "README.md"))
+    with open(os.path.join(corelibrary, "corelibrary.py"), "w") as fp:
+        print("TWO = 2", file=fp)
+    subprocess.check_call(args=[poetry, "init", "--no-interaction"], cwd=corelibrary)
+
     anotherlibrary = os.path.join(str(tmpdir), "anotherlibrary")
     touch(os.path.join(anotherlibrary, "README.md"))
     with open(os.path.join(anotherlibrary, "anotherlibrary.py"), "w") as fp:
-        print("MEANING_OF_LIFE = 42", file=fp)
-    subprocess.check_call(args=[poetry, "init", "--no-interaction"], cwd=anotherlibrary)
+        fp.write(
+            dedent(
+                """\
+                from corelibrary import TWO
+
+
+                MEANING_OF_LIFE = TWO * 21
+                """
+            )
+        )
+    subprocess.check_call(
+        args=[poetry, "init", "--no-interaction", "--dependency", "../corelibrary"],
+        cwd=anotherlibrary,
+    )
 
     mylibrary = os.path.join(str(tmpdir), "mylibrary")
     touch(os.path.join(mylibrary, "README.md"))
@@ -59,9 +77,26 @@ def test_wheel_file_url_dep(tmpdir):
     assert len(wheels) == 1
     wheel = wheels[0]
 
+    pex_root = os.path.join(str(tmpdir), "pex_root")
     testing_pex = os.path.join(str(tmpdir), "testing.pex")
     run_pex_command(
-        args=[wheel, "-e", "mylibrary:deep_thought", "-o", testing_pex]
+        args=[
+            "--pex-root",
+            pex_root,
+            "--runtime-pex-root",
+            pex_root,
+            "--resolver-version",
+            "pip-2020-resolver",
+            # N.B.: Modern Pip is needed to handle Poetry relative path deps. Older Pip does
+            # building off in a tmp dir and that breaks relative path references.
+            "--pip-version",
+            "22.3",
+            wheel,
+            "-e",
+            "mylibrary:deep_thought",
+            "-o",
+            testing_pex,
+        ]
     ).assert_success()
     assert (
         "MEANING_OF_LIFE = 42"
