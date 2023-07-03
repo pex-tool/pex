dbc04805b68227033ca48e3994595d8fffb2b80b
John Sirois <john.sirois@gmail.com>
2023-07-03 08:00:26 -0700

    Limit the special 3.12 beta handling to the install command and 3.12.

--- scripts/install_command.sh
+++ scripts/install_command.sh
@@ -3,7 +3,9 @@
 if [[ "3.12" = "$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')" ]]
 then
   python -sE -m pip install -U \
-    "pip @ git+https://github.com/pypa/pip@8a1eea4aaedb1fb1c6b4c652cd0c43502f05ff37"
+    "pip @ git+https://github.com/pypa/pip@8a1eea4aaedb1fb1c6b4c652cd0c43502f05ff37" \
+    setuptools \
+    wheel
 fi
 
 exec python -sE -m pip install "$@"
