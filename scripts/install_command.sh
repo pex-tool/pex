fd18d15b8e3aa96398c18891014f27b513dd3b1d
John Sirois <john.sirois@gmail.com>
2023-07-02 18:41:49 -0700

    Try another technique.

--- scripts/install_command.sh
+++ scripts/install_command.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -n "${_PEX_TOX_INSTALL_COMMAND_PIP_REQUIREMENT}"  ]]; then
-  python -sE -m pip install -U "${_PEX_TOX_INSTALL_COMMAND_PIP_REQUIREMENT}"
+if [[ "3.12" = "$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')" ]]
+then
+  python -sE -m pip install -U \
+    "pip @ git+https://github.com/pypa/pip@8a1eea4aaedb1fb1c6b4c652cd0c43502f05ff37"
 fi
 
 exec python -sE -m pip install "$@"
