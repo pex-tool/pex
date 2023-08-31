f1dbaf1a51e84c69922cb17011017fa03e02df75
John Sirois <john.sirois@gmail.com>
2023-08-31 10:29:08 -0700

    Update the base image.
    Include more and more modern Pythons to cover our supported list for
    both local testing and CI. Also include cargo in the image which is
    needed for some projects sdist builds of native code.

--- docker/base/install_pythons.sh
+++ docker/base/install_pythons.sh
@@ -7,16 +7,20 @@ export PYENV_ROOT=/pyenv
 # N.B.: The 1st listed version will supply the default `python` on the PATH; otherwise order does
 # not matter.
 PYENV_VERSIONS=(
-  3.11.4
+  3.11.5
   2.7.18
   3.5.10
   3.6.15
   3.7.17
-  3.8.17
-  3.9.17
-  3.10.12
+  3.8.18
+  3.9.18
+  3.10.13
   3.12.0rc1
   pypy2.7-7.3.12
+  pypy3.5-7.0.0
+  pypy3.6-7.3.3
+  pypy3.7-7.3.9
+  pypy3.8-7.3.11
   pypy3.9-7.3.12
   pypy3.10-7.3.12
 )
