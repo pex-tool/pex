36dd2374569b5f3fbef54918e6ba3bd1ff852b61
John Sirois <john.sirois@gmail.com>
2024-05-12 19:58:41 -0500

    Un-pin hatchling. (#2408)
    Although hatchling fixed a few issues that broke Pex packaging, it has
    since more strictly adhered to PEP-621 which breaks the existing project
    metadata customization scheme. Update the scheme to conform to PEP-621
    strictures. Also update the tox configuration to work with modern
    hatchling.
    
    Picks up from #2403 by @ofek

--- build-backend/pex_build/hatchling/metadata_hook.py
+++ build-backend/pex_build/hatchling/metadata_hook.py
@@ -32,30 +32,71 @@ class AdjustMetadata(MetadataHookInterface):
 
     The following mutations are supported:
     + Specifying alternate requires-python metadata via _PEX_REQUIRES_PYTHON env var.
-    + Expanding format string placeholder (`{name}`) with metadata values via the `expand` mapping of placeholder name
-      to metadata value.
+    + Expanding format string placeholder (`{name}`) with metadata values via the `expand` mapping
+      of placeholder name to metadata value.
     """
 
     PLUGIN_NAME = "pex-adjust-metadata"
 
     def update(self, metadata):
         # type: (Dict[str, Any]) -> None
-        requires_python = os.environ.get("_PEX_REQUIRES_PYTHON")
-        if requires_python:
+
+        dynamic_metadata = self.config.get("project")
+        if not dynamic_metadata:
+            return
+
+        self._update_requires_python(metadata, dynamic_metadata)
+        self._expand_placeholders(metadata, dynamic_metadata)
+
+    def _update_requires_python(
+        self,
+        metadata,  # type: Dict[str, Any]
+        dynamic_metadata,  # type: Dict[str, Any]
+    ):
+        # type: (...) -> None
+
+        dynamic_requires_python = os.environ.get("_PEX_REQUIRES_PYTHON")
+        static_requires_python = dynamic_metadata.get("requires-python")
+
+        if dynamic_requires_python:
+            if not static_requires_python:
+                raise ValueError(
+                    "A dynamic override of requires-python metadata was specified via "
+                    "`_PEX_REQUIRES_PYTHON={dynamic_requires_python}` but there was no "
+                    "corresponding static value defined in "
+                    "`tool.hatch.metadata.hooks.{plugin_name}.project`.".format(
+                        plugin_name=self.PLUGIN_NAME,
+                        dynamic_requires_python=dynamic_requires_python,
+                    )
+                )
+
             print(
                 "pex_build: Dynamically modifying pyproject.toml requires-python of {original} to "
-                "{dynamic}".format(original=metadata["requires-python"], dynamic=requires_python),
+                "{dynamic}".format(
+                    original=static_requires_python, dynamic=dynamic_requires_python
+                ),
                 file=sys.stderr,
             )
-            metadata["requires-python"] = requires_python
+            dynamic_metadata["requires-python"] = dynamic_requires_python
+
+        if dynamic_metadata["requires-python"]:
+            metadata["requires-python"] = dynamic_metadata["requires-python"]
+
+    def _expand_placeholders(
+        self,
+        metadata,  # type: Dict[str, Any]
+        dynamic_metadata,  # type: Dict[str, Any]
+    ):
+        # type: (...) -> None
 
         expand = self.config.get("expand")
-        if expand:
-            metadata.update(
-                (
-                    key,
-                    expand_value(value, **{key: metadata[value] for key, value in expand.items()}),
-                )
-                for key, value in metadata.items()
-                if key != "version"
+        if not expand:
+            return
+
+        metadata.update(
+            (
+                key,
+                expand_value(value, **{key: metadata[value] for key, value in expand.items()}),
             )
+            for key, value in dynamic_metadata.items()
+        )
