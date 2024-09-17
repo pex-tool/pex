30ce5425dd52947ded09652506c644fab347d5f5
John Sirois <john.sirois@gmail.com>
2024-09-17 08:49:43 -0700

    Re-work testing pyenv management.
    The new setup allows for safe upgrades by just bumping pyenv Python
    versions. Also re-work atomic_directory to support this cleanly, allow
    obtaining an independent lock on an atomic directory after it has been
    established. This can be useful when reads of the directory contents all
    happen in the same code path as the writes.

--- testing/__init__.py
+++ testing/__init__.py
@@ -539,22 +539,14 @@ def ensure_python_distribution(version):
 
     pip = os.path.join(interpreter_location, "bin", "pip")
 
-    with atomic_directory(target_dir=pyenv_root) as target_dir:
-        if not target_dir.is_finalized():
-            bootstrap_python_installer(target_dir.work_dir)
-
     with atomic_directory(target_dir=interpreter_location) as interpreter_target_dir:
         if not interpreter_target_dir.is_finalized():
-            subprocess.check_call(
-                [
-                    "git",
-                    "--git-dir={}".format(os.path.join(pyenv_root, ".git")),
-                    "--work-tree={}".format(pyenv_root),
-                    "reset",
-                    "--hard",
-                    "v2.3.8",
-                ]
-            )
+            with atomic_directory(target_dir=pyenv_root) as target_dir:
+                if target_dir.is_finalized():
+                    with target_dir.locked():
+                        subprocess.check_call(args=["git", "pull", "--ff-only"], cwd=pyenv_root)
+                else:
+                    bootstrap_python_installer(target_dir.work_dir)
             env = pyenv_env.copy()
             if sys.platform.lower().startswith("linux"):
                 env["CONFIGURE_OPTS"] = "--enable-shared"
