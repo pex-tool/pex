3cc2b6350de84405536c4d3db5c34b6996e451ab
John Sirois <john.sirois@gmail.com>
2024-10-26 18:24:35 -0700

    Fix implicit relavtive import under Python 2.7.

--- testing/scie.py
+++ testing/scie.py
@@ -1,5 +1,8 @@
 # Copyright 2024 Pex project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import
+
 import pytest
 
 from pex.scie import SciePlatform
