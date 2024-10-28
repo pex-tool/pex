04192c19fc3e630de7c62b369b0bea7b45f033aa
John Sirois <john.sirois@gmail.com>
2024-10-28 16:40:32 -0700

    Vendor tomli/tomli-w.

---
+++ pex/vendor/_vendored/tomli-w/tomli_w/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ("dumps", "dump")
+__version__ = "1.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+
+if "tomli-w" in __import__("os").environ.get("__PEX_UNVENDORED__", ""):
+  from tomli_w._writer import dump, dumps  # vendor:skip
+else:
+  from pex.third_party.tomli_w._writer import dump, dumps
+
