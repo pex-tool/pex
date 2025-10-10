570a1d5161197e61fb9f0b98c7dfa05973b4ed8b
John Sirois <john.sirois@gmail.com>
2025-10-10 09:36:41 -0700

    Switch to deadsnakes, fix default python; fix lint command.

--- scripts/lint.py
+++ scripts/lint.py
@@ -24,6 +24,7 @@ def run_autoflake(*args: str) -> None:
             "--exclude",
             ",".join(excludes),
             "--recursive",
+            "-v",
             "build-backend",
             "docs",
             "package",
