9e9951cc98d67dd52619b8ac49dc6267b9bbd8a8
John Sirois <john.sirois@gmail.com>
2025-11-04 09:38:24 -0800

    Upgrade `science` to 0.15.1. (#2995)
    Fixes #2994

--- pex/scie/science.py
+++ pex/scie/science.py
@@ -68,7 +68,7 @@ class Manifest(object):
 
 
 SCIENCE_RELEASES_URL = "https://github.com/a-scie/lift/releases"
-MIN_SCIENCE_VERSION = Version("0.15.0")
+MIN_SCIENCE_VERSION = Version("0.15.1")
 SCIENCE_REQUIREMENT = SpecifierSet("~={min_version}".format(min_version=MIN_SCIENCE_VERSION))
 
 
