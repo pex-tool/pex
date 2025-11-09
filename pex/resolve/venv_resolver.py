7cd2680b002cea0b56bf3c8e57ec3a695d0128fe
John Sirois <john.sirois@gmail.com>
2025-11-08 19:09:24 -0800

    Fix fmt.

--- pex/resolve/venv_resolver.py
+++ pex/resolve/venv_resolver.py
@@ -542,8 +542,9 @@ def _resolve_from_venv(
             requirements=root_requirements,
             constraints_by_project_name=constraints_by_project_name,
             dependency_configuration=dependency_configuration,
-            allow_prereleases=root_requirements_has_prereleases
-            or pip_configuration.allow_prereleases,
+            allow_prereleases=(
+                root_requirements_has_prereleases or pip_configuration.allow_prereleases
+            ),
             compile=compile,
             ignore_errors=ignore_errors,
             result_type=result_type,
