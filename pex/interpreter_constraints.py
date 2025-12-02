b1d1dbc039dc79cd10f64faa1f22b2d252aabbf4
John Sirois <john.sirois@gmail.com>
2025-12-02 17:40:40 -0600

    Upgrade duvrc image to CPython 3.14.1.

--- pex/interpreter_constraints.py
+++ pex/interpreter_constraints.py
@@ -380,7 +380,7 @@ COMPATIBLE_PYTHON_VERSIONS = (
     PythonVersion(Lifecycle.STABLE, 3, 11, 14),
     PythonVersion(Lifecycle.STABLE, 3, 12, 12),
     PythonVersion(Lifecycle.STABLE, 3, 13, 9),
-    PythonVersion(Lifecycle.STABLE, 3, 14, 0),
+    PythonVersion(Lifecycle.STABLE, 3, 14, 1),
     PythonVersion(Lifecycle.DEV, 3, 15, 0),
 )
 
