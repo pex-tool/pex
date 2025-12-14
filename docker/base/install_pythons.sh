27ed6a159f650c032dff22e1b05e4be3bc912f7f
John Sirois <john.sirois@gmail.com>
2025-12-14 13:09:39 -0800

    Switch 3.1{3,4} from pyenv to deadsnakes. (#3041)

--- docker/base/install_pythons.sh
+++ docker/base/install_pythons.sh
@@ -15,6 +15,8 @@ DEADSNAKES_VERSIONS=(
   "3.9 {dev,venv,distutils}=3.9.25"
   "3.10 {dev,venv,distutils}=3.10.19"
   "3.11 {dev,venv,distutils}=3.11.14"
+  "3.13 {dev,venv}=3.13.11"
+  "3.14 {dev,venv}=3.14.2"
   "3.15 {dev,venv}=3.15.0~a2"
 )
 
@@ -34,8 +36,6 @@ PYENV_VERSIONS=(
   2.7.18
   3.5.10
   3.6.15
-  3.13.11
-  3.14.2
   pypy2.7-7.3.20
   pypy3.5-7.0.0
   pypy3.6-7.3.3
