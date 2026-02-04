4860472f87975663c95b5126193a1a2d3ad725cf
John Sirois <john.sirois@gmail.com>
2026-02-03 22:15:06 -0800

    Upgrade to CPython 3.14.3 and 3.13.12. (#3097)

--- docker/base/install_pythons.sh
+++ docker/base/install_pythons.sh
@@ -15,8 +15,6 @@ DEADSNAKES_VERSIONS=(
   "3.9 {dev,venv,distutils}=3.9.25"
   "3.10 {dev,venv,distutils}=3.10.19"
   "3.11 {dev,venv,distutils}=3.11.14"
-  "3.13 {dev,venv}=3.13.11"
-  "3.14 {dev,venv}=3.14.2"
 )
 
 add-apt-repository --yes --ppa deadsnakes
@@ -35,6 +33,8 @@ PYENV_VERSIONS=(
   2.7.18
   3.5.10
   3.6.15
+  3.13.12
+  3.14.3
   3.15.0a5
   pypy2.7-7.3.20
   pypy3.5-7.0.0
