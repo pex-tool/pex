690b495ac4a00bb5ce029e4e4c5cc719a59e20fa
John Sirois <john.sirois@gmail.com>
2026-08-04 17:50:55 -0700

    Upgrade to CPythons to 3.15.0rc1, 3.14.7 & 3.13.15.

--- pex/interpreter_constraints.py
+++ pex/interpreter_constraints.py
@@ -396,8 +396,8 @@ COMPATIBLE_PYTHON_VERSIONS = (
     PythonVersion(Lifecycle.STABLE, 3, 10, 20),
     PythonVersion(Lifecycle.STABLE, 3, 11, 15),
     PythonVersion(Lifecycle.STABLE, 3, 12, 13),
-    PythonVersion(Lifecycle.STABLE, 3, 13, 14),
-    PythonVersion(Lifecycle.STABLE, 3, 14, 6),
+    PythonVersion(Lifecycle.STABLE, 3, 13, 15),
+    PythonVersion(Lifecycle.STABLE, 3, 14, 7),
     PythonVersion(Lifecycle.DEV, 3, 15, 0),
 )
 
